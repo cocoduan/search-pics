8328a8f123b5df2669efc59d3ae2ea8c38479fd4
Coco Duan <dhh1216@gmail.com>
2020-08-09 18:48:30 -0700

    turn SearchBar into a functional component

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,28 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-class SearchBar extends React.Component {
-	state = { term: '' };
-	onInputChange = (event) => {
-		const term = event.target.value;
-		this.setState({ term });
+const SearchBar = (props) => {
+	const [term, setTerm] = useState('');
+	const onInputChange = (event) => {
+		setTerm(event.target.value);
 	}
-	onFormSubmit = (event) => {
+	const onFormSubmit = (event) => {
 		// keep the browser from page refreshing when pressing enter
 		event.preventDefault();
-		this.props.onSubmit(this.state.term);
+		props.onSubmit(term);
 	}
-	render() {
-		return (
-			<div className="ui segment">
-				<form className="ui form" onSubmit={this.onFormSubmit}>
-					<div className="field">
-						<label>Image Search</label>
-						<input type="text" value={this.state.term} onChange={this.onInputChange}></input>
-					</div>
-				</form>
-			</div>
-		)
-	}
+	return (
+		<div className="ui segment">
+			<form className="ui form" onSubmit={onFormSubmit}>
+				<div className="field">
+					<label>Image Search</label>
+					<input type="text" value={term} onChange={onInputChange}></input>
+				</div>
+			</form>
+		</div>
+	)
 }
 
 export default SearchBar;
