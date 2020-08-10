642cc0f36503a3541c691105911a4f16b0ad430b
Coco Duan <dhh1216@gmail.com>
2020-08-09 23:30:26 -0700

    fix indention using 2 spaces

--- src/api/unsplash.js
+++ src/api/unsplash.js
@@ -2,23 +2,23 @@ import axios from 'axios';
 import { UNSPLASH_URL, ACCESS_KEY, SEARCH_PHOTOS } from '../constants';
 
 const unsplash = axios.create({
-	baseURL: UNSPLASH_URL,
-	headers: {
-		Authorization: `Client-ID ${ACCESS_KEY}`,
-	}
+  baseURL: UNSPLASH_URL,
+  headers: {
+    Authorization: `Client-ID ${ACCESS_KEY}`,
+  }
 });
 
 export const getImages = async (term) => {
-	try {
-		const response = await unsplash.get(SEARCH_PHOTOS, {
-			params: { query: term }
-		});
-		if (response) {
-			return response.data.results;
-		}
-		return Promise.reject('Failed to get images.');
-	} catch (e) {
-		console.log(`Failed to get images with expcetion:${e}.`);
-	}
+  try {
+    const response = await unsplash.get(SEARCH_PHOTOS, {
+      params: { query: term }
+    });
+    if (response) {
+      return response.data.results;
+    }
+    return Promise.reject('Failed to get images.');
+  } catch (e) {
+    console.log(`Failed to get images with expcetion:${e}.`);
+  }
 }
 export default unsplash;
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,25 +1,25 @@
 import React, { useState } from 'react';
 
 const SearchBar = (props) => {
-	const [term, setTerm] = useState('');
-	const onInputChange = (event) => {
-		setTerm(event.target.value);
-	}
-	const onFormSubmit = (event) => {
-		// keep the browser from page refreshing when pressing enter
-		event.preventDefault();
-		props.onSubmit(term);
-	}
-	return (
-		<div className="ui segment">
-			<form className="ui form" onSubmit={onFormSubmit}>
-				<div className="field">
-					<label>Image Search</label>
-					<input type="text" value={term} onChange={onInputChange}></input>
-				</div>
-			</form>
-		</div>
-	)
+  const [term, setTerm] = useState('');
+  const onInputChange = (event) => {
+    setTerm(event.target.value);
+  }
+  const onFormSubmit = (event) => {
+    // keep the browser from page refreshing when pressing enter
+    event.preventDefault();
+    props.onSubmit(term);
+  }
+  return (
+    <div className="ui segment">
+      <form className="ui form" onSubmit={onFormSubmit}>
+        <div className="field">
+          <label>Image Search</label>
+          <input type="text" value={term} onChange={onInputChange}></input>
+        </div>
+      </form>
+    </div>
+  )
 }
 
 export default SearchBar;
